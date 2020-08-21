de585394e0772a946466d76979c0f280edfd8e7c
Rishabh Goel <rgoel1999@gmail.com>
2020-08-21 17:14:48 +0530

    Relatively Big update!
    This update contains the necessary code for generation of coordinates
    for the computer's ships. Now both the players are able to place their
    ships on the board. Next thing to implement is the actual play.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-import math
+from random import randint, choice
+from ships import *
+
+#Text to help the player with the how-to of the game
 
 def ui():
+    """Includes How-to-play of the original board game.
+       Just some stuff to be printed. Not much!"""
 
     print("Hello!")
 
@@ -34,7 +39,9 @@ def ui():
 
 # Display the map
 
-def grid(lst):
+def show_grid(lst):
+    """Shows the grid on the screen"""
+
     print("\nThis is how the 10*10 grid looks\n")
     print("   0 1 2 3 4 5 6 7 8 9\n")
     for index, x in enumerate(lst):
@@ -42,31 +49,111 @@ def grid(lst):
     return 0
 
 
-# Player interaction
+# List of all the ships and their size
 
-def player():
-    list_ships = ["Carrier", "Battleship", "Destroyer", "Submarine", "Patrol Boat"]
-    print("-------------Let's begin then-------------------\n")
-    print("This is how the 10*10 grid looks\n")
+list_ships = [
+    ("Carrier", 5),
+    ("Battleship", 4),
+    ("Destroyer", 3),
+    ("Submarine", 3),
+    ("Patrol Boat", 2)
+]
+
+# Generates a 10*10 grid
+
+def load_grid():
+    """Creates a 10*10 grid for use later in the other functions"""
+
     lst = []
     for x in range(10):
-        lst.append([0,0,0,0,0,0,0,0,0,0])
-    print("   0 1 2 3 4 5 6 7 8 9\n")
-    for index, x in enumerate(lst):
-        print(str(index), " ".join(map(str, x)), sep="  ")
+        lst.append(['-','-','-','-','-','-','-','-','-','-'])
+    return lst
 
+# Player Interaction
+
+def player():
+    """UI for the input from the player for the co-ordinates of the ships"""
+
+    print("-------------Let's begin then-------------------\n")
+    print("This is how the 10*10 grid looks\n")
+    lst = load_grid()
+    grid_with_zero(lst)
     print("\nNow it is you turn to play:\n")
-    for ship in list_ships:
-        print(f"Let's place {ship}:\n")
-        while(True):
+    p = Player(input("Enter your name:"))
+    print("\nTry not to place the ships on the same place\n")
+    for ship, size in list_ships:
+        print(f"Let's place {ship} of size: {size}\n")
+        s = Ships(ship, size)
+        ship_placed = False
+        while not ship_placed:
             r = int(input("Choose a value between 0-9 to choose the row:"))
             c = int(input("Choose a value between 0-9 to choose a column:"))
             o = input("Place horizontal(h) or vertical(v)?")
-            lst[r][c] = 1
-            break
-        break
-        grid(lst)
-    return 0
+            if o == 'h':
+                if c + s.size <= 9:
+                    s.set_pos(r,r,c, c+s.size)
+                    for grid_col in range(s.size):
+                        lst[r][c] = 1
+                        p.ship_coordinates.append((r,c))
+                        c+=1
+                    ship_placed = True
+                else:
+                    print("The size of the ship is too big, try again!")
+            elif o == 'v':
+                if r + s.size <= 9:
+                    s.set_pos(r,r+s.size, c,c)
+                    for grid_row in lst[r:r+s.size]:
+                        grid_row[c] = 1
+                        p.ship_coordinates.append((r,c))
+                    ship_placed = True
+                else:
+                    print("The size of the ship is too big, try again!")
+            else:
+                print("Enter h or v for horizontal or vertical")
+        p.dict_ships[ship] = s
+    show_grid(lst)
+    return p
+
+# Generating values for computer ships
+
+def comp():
+    """Generates coordinates for the computer's ships"""
+
+    print("\nWait for the Computer to place it's ships\n")
+    lst = load_grid()
+    p = Player("Computer")
+    for ship, size in list_ships:
+        s = Ships(ship, size)
+        ship_placed = False
+        while not ship_placed:
+            r = randint(0,9)
+            c = randint(0,9)
+            o = choice(['h','o'])
+            if o == 'h':
+                if c + s.size <= 9:
+                    s.set_pos(r,r,c, c+s.size)
+                    for grid_col in range(s.size):
+                        lst[r][c] = 1
+                        p.ship_coordinates.append((r,c))
+                        c+=1
+                    ship_placed = True
+                else:
+                    continue
+            else:
+                if r + s.size <= 9:
+                    s.set_pos(r,r+s.size, c,c)
+                    for grid_row in lst[r:r+s.size]:
+                        grid_row[c] = 1
+                        p.ship_coordinates.append((r,c))
+                    ship_placed = True
+                else:
+                    continue
+        p.dict_ships[ship] = s
+    show_grid(lst)
+    return p
+
+# Call the necessary functions
 
 if __name__ == '__main__':
-    player()
+    # play = player()
+    comp = comp()
--- ships.py
+++ ships.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
 class Ships:
-    def __init__(self, name, size, orientation='h'):
+    def __init__(self, name, size):
         self.name = name
         self.size = size
-        self.orient = 'h'
 
-    def set_pos(self, row, column, orientation='h'):
-        self.row = row
-        self.column = column
-        if orientation == 'h':
-            self.orient = 'h'
-        else:
-            self.orient = 'v'
+    def set_pos(self, row_start, row_end, column_start, column_end, orientation='h'):
+        self.row_start = row_start
+        self.row_end = row_end
+        self.column_start = column_start
+        self.column_end = column_end
+        self.orient = 'h' if orientation == 'h' else 'v'
 
     def get_pos(self):
-        return (self.name, self.row, self.column, self.orient)
+        return (self.name, self.row_start, self.row_end,
+                self.column_start, self.column_end, self.orient)
 
     def __repr__(self):
         return f"{self.name} has size {self.size}"
+
+class Player:
+    def __init__(self, player_name="Player1"):
+        self.player_name = player_name
+        self.ship_coordinates = list()
+        self.dict_ships = dict()
+
+    def __repr__(self):
+        return f"Player name: {self.player_name}"
